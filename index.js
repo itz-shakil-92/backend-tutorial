db2f6dfd1e1bbc5120c3f89100af847e21d80891
itz-shakil-92 <shakilkathat5603@gmail.com>
2024-09-22 21:38:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/twitter', (req, res) => {
+    res.send('Twitter: iamshakilkathat')
+  })
+
+  app.get('/login', (req, res) => {
+    res.send("<h1>Hello Bhaijaan !! Pehle Login Krloooo</h1>")
+  })  
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
